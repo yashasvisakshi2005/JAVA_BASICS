ba0a9b81c72f513cf79037098695d705ea9028b7
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-08 13:02:43 +0000

    basic question

---
+++ Multiplicatontable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+       int n = s.nextInt();
+       for(int i = 1 ; i<=10 ;i++){
+           int p = n*i;
+           System.out.println(n + " X " + i + " = " + p);
+       }
+	}
+}
