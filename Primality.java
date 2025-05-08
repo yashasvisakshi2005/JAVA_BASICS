d5d6be9b3a8b8bb3af755221c33f155d2bb3c692
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-08 12:54:02 +0000

    basic question

---
+++ Primality.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+       int n = s.nextInt();
+      int p = 1;
+	  for(int i=2;i<n-1;i++){
+	      if(n%i==0){
+	        System.out.println(i);  
+	      }
+	      p=n;
+	  }
+	  if(p==0){
+		System.out.println("PRIME");
+	  }else{
+	      System.out.println(" NOT PRIME");
+	  }
+	}
+}
