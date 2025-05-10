6dfa0c2bfb50cea340df4a54ba649ecf4ef13994
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-10 07:40:05 +0000

    basic question

---
+++ Gp.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int r = sc.nextInt();
+	    int n = sc.nextInt();
+	    for(int i=1 ;i<=n;i++){
+	        System.out.println(a+" "); 
+	         a*=r;
+	    }
+	   
+	   
+		
+	}
+}
