7affb52462cc2a458767892c12e98c2012b7badc
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-10 05:52:55 +0000

    basic question

---
+++ Highestfactor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for(int i =n-1;i>=1;i--){
+	        if(n%i==0){
+	        System.out.println(i);    
+	        break;
+	        }
+	    }
+		
+	}
+}
