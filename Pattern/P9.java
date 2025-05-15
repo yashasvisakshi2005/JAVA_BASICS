25d140502660a7b06c4947874ecd53178705ef0d
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-15 12:12:16 +0000

    all pattern question

---
+++ Pattern/P9.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	   
+	    for(int i=1;i<=n;i++){
+	        int k = i;
+	        
+	        for(int j=1;j<=n;j++){
+	            System.out.print((char)(64+k));
+	          k++;
+	        }
+	        System.out.println( );
+	    }
+// 		System.out.println("Hello World");
+	}
+}
