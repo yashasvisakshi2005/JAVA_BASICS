5ac7e193d658b8032427119234e852c559e74d96
yashasvisakshi2005 <yashasvisakshi2005@gmail.com>
2025-05-16 17:17:56 +0000

    Advance pattern questions

---
+++ Pattern/P11.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=n;j++){
+	            if(i+j<=n) System.out.print(" "); 
+	           else System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+// 		System.out.println("Hello World");
+	}
+}
---
+++ Pattern/P13.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for(int i=n;i>=1;i--){
+	       for(int j=1;j<=n-i+1;j++)
+            System.out.print(i+j-1); 
+	           
+	        
+	        System.out.println();
+	    }
+// 		System.out.println("Hello World");
+	}
+}
